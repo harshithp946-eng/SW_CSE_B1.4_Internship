dc23c5c61a4675581b95e8be7fcc761be1fc88b2
harshithp946-eng <harshithp946@gmail.com>
2026-07-29 12:27:30 +0530

    Imported the components and rendered it

--- Day/Day13/day13/src/App.jsx
+++ Day/Day13/day13/src/App.jsx
@@ -1,42 +1,35 @@
-import Button from './components/FunctionalComponent';
+import Button from './components/FunctionalComponent'
 import { LeftArrow,RightArrow } from "./components/FunctionalComponentTwo";
 const name = "Harshith";
 const isLoggedIn = false;
 
 function App() {
-  
+ 
   return (
-    //React fragment
+    // React Fragments
     <>
-    {
-      /*JSX
-      Rule 1:Return one parent element
-    */}
-    <h1>Hello </h1>
-    <p>Hello again!</p>
-    {
-      /*JSX
-      Rule 2:Every tag must be closed properly
-    */}
-    <p>Error</p>
-    <input type="text" />
-{
-      /*JSX
-      Rule 3:Use camelCase for HTML attributes
-    */}
-    <button onClick onSubmit> Click </button>
-    {
-      /*JSX
-      Rule 4:Embedding Javascript with curly braces
-    */}
-    <h3>Hello, {name}</h3>
-    {
-      /*JSX
-      Rule 5:Use expressions,not statements 
-    */}
-    <p>{isLoggedIn ? "Welcome back!" : "Please login."}</p>
-    <Button />
+      {/* JSX Rule 1: Return one parent element */}
+      <h1>Hello</h1>
+      <p>Hello again</p>
+
+      {/* JSX Rule 2: Every tag must be closed properly */}
+      <p>Error</p>
+      <input type="text" />
+
+      {/* JSX Rule 3: use camelCase for attributes */}
+      <button onClick onSubmit>Click</button>
+
+      {/* JSX Rule 4: Embedding JavaScript with curly braces */}
+      <h3>Hello, {name}</h3>
+
+      {/* JSX Rule 5: use expressions, not statements */}
+      <p>{isLoggedIn ? "Welcome back" : "Please Login"}</p>
+
+      <Button />
+      <LeftArrow />
+      <RightArrow />
     </>
+    
   )
 }
 
