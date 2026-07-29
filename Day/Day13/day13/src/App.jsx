6d7330554bcd10adb68ee6247b751081ed656df0
harshithp946-eng <harshithp946@gmail.com>
2026-07-29 15:01:05 +0530

    State ex3: state updation using form input

--- Day/Day13/day13/src/App.jsx
+++ Day/Day13/day13/src/App.jsx
@@ -7,9 +7,10 @@ import InfoBoxDemo from "./components/PropsTwo";
 import './App.css'
 
 import Counter from "./components/State";
-
 import ToggleText from "./components/StateTwo";
-const name = "Harshith";
+import NameInput from "./components/StateThree";
+
+const name = "Amay";
 const isLoggedIn = false;
 
 function App() {
@@ -45,6 +46,8 @@ function App() {
       <Counter />
 
       <ToggleText />
+
+      <NameInput />
     </>
     
   )
