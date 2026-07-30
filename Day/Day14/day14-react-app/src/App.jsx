1ce219c4b73526ef5bbe9369bd93a8f5b45a08d1
harshithp946-eng <harshithp946@gmail.com>
2026-07-30 15:59:37 +0530

    Form ex2

--- Day/Day14/day14-react-app/src/App.jsx
+++ Day/Day14/day14-react-app/src/App.jsx
@@ -6,7 +6,8 @@ import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
 import SimpleNameForm from "./components/FormOne";
-
+import ValidatedForm from "./components/FormTwo";
+import ValidatedForm from "./components/FormThree";
 
 
 function App() {
@@ -19,7 +20,9 @@ function App() {
        <CounterHook/> */}
        {/* <UserListHook/> */}
         {/* <ThemeProviderDemo /> */}
-        <SimpleNameForm />
+        {/* <SimpleNameForm /> */}
+         {/* <SignupForm /> */}
+          <ValidatedForm/>
     </>
   )
 }
